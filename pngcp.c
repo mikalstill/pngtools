ecf43a8d16ee7325d1eb28f104e1e762f0ea70ea
mikal <mikal@e7f0f05e-9a5c-0410-9775-f07f60351c7c>
2003-01-07 04:06:10 +0000

    Mikal: Very nearly finished the first cut of pngcp, but the scaling factor calculation is currently wrong if you are copying an image a given config to the same config

--- pngcp.c
+++ pngcp.c
@@ -1,13 +1,16 @@
 #include <unistd.h>
 #include <stdio.h>
+#include <math.h>
 #include "pngcp.h"
 
 void usage();
 
 int main(int argc, char *argv[]){
-  unsigned long width, height, inset;
-  int channels, targetchannels = 3, bitdepth, targetbitdepth = 8, optchar, i;
-  char *raster;
+  unsigned long width, height, inset, outset;
+  int channels, targetchannels = -1, bitdepth, targetbitdepth = -1, optchar, i,
+    bytedepth, targetbytedepth;
+  char *input, *output;
+  float scalefactor;
   
   i = 1;
   while ((optchar = getopt (argc, argv, "d:s:")) != -1)
@@ -37,18 +40,51 @@ int main(int argc, char *argv[]){
 
   // Colour depth is the number of bits per sample
   // Bit depth is the number of samples per pixel
-  if((raster = pngcp_readimage(argv[i], &width, &height, &bitdepth, &channels)) == -1){
+  if((input = pngcp_readimage(argv[i], &width, &height, &bitdepth, &channels)) == -1){
     fprintf(stderr, "Failed to read the input raster\n");
     exit(42);
   }
+  if(targetbitdepth == -1) targetbitdepth = bitdepth;
+  if(targetchannels == -1) targetchannels = channels;
 
-  printf("Bitdepth = %d, channels = %d\n", bitdepth, channels);
-  printf("Target bitdepth = %d, target channels = %d\n", targetbitdepth, targetchannels);
+  // Calculate the byte depth
+  bytedepth = bitdepth / 8;
+  if(bitdepth % 8 != 0)
+    bytedepth++;
   
-  // Work through the input pixels, and turn them into output pixels
-  for(inset = 0; inset = width * height; inset++){
-    
+  // Calculate the target byte depth
+  targetbytedepth = targetbitdepth / 8;
+  if(targetbitdepth % 8 != 0)
+    targetbytedepth++;
+  
+  // Build the output raster
+  if((output = (char *) malloc(width * height * targetchannels * targetbytedepth)) == NULL){
+    fprintf(stderr, "Failed to allocate enough memory for output raster\n");
+    exit(42);
+  }
 
+  printf("Bitdepth = %d, channels = %d, bytedepth = %d\n", bitdepth, channels, bytedepth);
+  printf("Target bitdepth = %d, target channels = %d, target bytedepth = %d\n", 
+	 targetbitdepth, targetchannels, targetbytedepth);
+  
+  // Determine how much each sample has to be scaled by to get to the new bitdepth
+  scalefactor = (float) (pow(2.0, (double) targetbitdepth) - pow(2.0, (double) bitdepth)) / 
+    pow(2.0, (double) bitdepth);
+  printf("Scaling factor is %f - %f / %f = %f\n", (float) pow(2.0, (double) targetbitdepth),
+	 (float) pow(2.0, (double) bitdepth), (float) pow(2.0, (double) bitdepth), scalefactor);
+
+  // Work through the input pixels, and turn them into output pixels
+  outset = 0;
+  for(inset = 0; inset < width * height * channels; inset += bytedepth){
+    // todo_mikal: This will only work for images with a bytedepth of one
+    output[outset] = input[inset] * scalefactor;
+    outset += targetbytedepth;
+  }
+
+  // Now push the raster into the output file
+  if(pngcp_writeimage(argv[i + 1], width, height, targetbitdepth, targetchannels, output) < 0){
+    fprintf(stderr, "Error writing the output file\n");
+    exit(42);
   }
 }
 
---
+++ pngwrite.c
@@ -0,0 +1,77 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <png.h>
+#include <unistd.h>
+#include "pngcp.h"
+
+int pngcp_writeimage(char *filename, unsigned long width, unsigned long height, 
+		      int bitdepth, int channels, char *raster){
+  FILE *image;
+  png_structp png;
+  png_infop info;
+  png_bytepp row_pointers = NULL;
+  int i, rowbytes;
+
+  if((image = fopen(filename, "wb")) == NULL){
+    fprintf(stderr, "Could not open the output image\n");
+    return -1;
+  }
+
+  // Determine how many bytes each row will consume
+  rowbytes = bitdepth / 8;
+  if(bitdepth % 8 != 0)
+    rowbytes++;
+  rowbytes *= channels;
+  rowbytes *= width;
+
+  // Convert the raster into a series of row pointers
+  if((row_pointers = malloc (height * sizeof (png_bytep))) == NULL){
+    fprintf(stderr, "Could not allocate memory\n");
+    return -1;
+  }
+
+  for (i = 0; i < height; ++i)
+    row_pointers[i] = raster + (i * rowbytes);
+
+  // Get ready for writing
+  if ((png =
+       png_create_write_struct (PNG_LIBPNG_VER_STRING, NULL, NULL,
+                                NULL)) == NULL){
+    fprintf(stderr, "Could not create write structure for PNG (out of memory?)");
+    return -1;
+  }
+
+  // The pixels are expanded to the nearest byte
+  png_set_expand (png);
+
+  // Get ready to specify important stuff about the image
+  if ((info = png_create_info_struct (png)) == NULL){
+    fprintf(stderr,
+            "Could not create PNG info structure for writing (out of memory?)");
+    return -1;
+  }
+
+  if (setjmp (png_jmpbuf (png))){
+    fprintf(stderr, "Could not set the PNG jump value for writing");
+    return -1;
+  }
+
+  // This is needed before IO will work (unless you define callbacks)
+  png_init_io(png, image);
+
+  // Define important stuff about the image
+  png_set_IHDR (png, info, width, height, bitdepth, PNG_COLOR_TYPE_RGB,
+                PNG_INTERLACE_NONE, PNG_COMPRESSION_TYPE_DEFAULT,
+                PNG_FILTER_TYPE_DEFAULT);
+  png_write_info (png, info);
+
+  // Write the image out
+  png_write_image (png, row_pointers);
+
+  // Cleanup
+  png_write_end (png, info);
+  png_destroy_write_struct (&png, &info);
+  fclose(image);
+  return 0;
+}
